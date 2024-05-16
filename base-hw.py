9f8e553e4f0ea22bd62a5ad1a1e0e68f7fc7dfea
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2024-05-17 00:32:50 +0200

    Add files via upload

---
+++ base-hw.py
@@ -0,0 +1,51 @@
+symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!']
+
+# Закодированное сообщение
+encoded_symbols = [18, 38, 46, 62, 66, 50, 33, 41, 66, 49, 48, 38, 58, 62, 68, 66, 48, 37, 42, 47, 66, 50, 33, 41, 66, 49, 48, 51, 49, 42, 67]
+
+# Раскодированное сообщение
+decoded_message = ''
+for i in range(len(encoded_symbols)):
+    decoded_message += f'{symbols[encoded_symbols[i]]}'
+print(decoded_message)
+
+
+
+
+# ЗАДАНИЕ №2
+
+
+expected_users = 1000
+
+users_by_day = [817, 1370, 752, 1247, 681, 1120, 915, 1281, 875, 1341, 757, 610, 812, 1170, 769, 1261, 845, 1289, 515,
+                1247, 845, 1311, 741, 1239, 812, 638, 877, 1242, 1159, 1372]
+
+all_u = 0
+max_u = 0
+min_u = users_by_day[0]
+
+
+for i in range(0, len(users_by_day)):
+    print('посещаемость в день', i + 1, users_by_day[i])
+    all_u += users_by_day[i]
+
+    if max_u < users_by_day[i]:
+        max_u = users_by_day[i]
+    elif min_u > users_by_day[i]:
+        min_u = users_by_day[i]
+
+
+sorted_u = [min_u]
+
+# for i in range(1, len(users_by_day)):
+#     index = users_by_day.index(min_u)
+#     sorted_u.append(users_by_day[index])
+#     users_by_day.pop(index)
+
+
+middle_u = all_u/len(users_by_day)
+
+print('Минимальное значение:', min_u)
+print('Максимальное значение:', max_u)
+print('Среднее значение:', middle_u)
+# print(sorted_u[0])
--- mnozhiteli-nyam-nyam-tasks.py
+++ mnozhiteli-nyam-nyam-tasks.py
@@ -1,4 +1,3 @@
-
 def mn1(n, m):
     if n > m:
         return 0
@@ -17,7 +16,6 @@ def mn2(n, m):
         return mn2(n+1, m) + mn2(n+2, m) + mn2(n*3, m)
 
 
-
 def mn3(n, m):
     if n > m:
         return 0
@@ -25,6 +23,15 @@ def mn3(n, m):
         return 1
     else:
         return mn3(n+4, m) + mn3(n+2, m)
+
+
+# def mn4(n, m):
+#     if 1 == 1:
+#         return 0
+#     elif n == m:
+#         return 1
+#     else:
+#         return mn4(n - 1, m) + mn4(n + 3, m) + mn4(n * 2, m)
 
 
 def mn5(n, m):
@@ -36,13 +43,31 @@ def mn5(n, m):
         return mn5(n+1, m) + mn5(n*4, m)
 
 
-def mn4(n, m):
-    if 1 == 1:
+def mn6(n, m):
+    if n > m:
         return 0
     elif n == m:
         return 1
     else:
-        return mn4(n-1, m) + mn4(n+3, m) + mn4(n*2, m)
+        return mn6(n + 1, m) + mn6(n * 2, m)
+
+
+def mn7(n, m):
+    if n > m:
+        return 0
+    elif n == m:
+        return 1
+    else:
+        return mn7(n + 1, m) + mn7(n + 2, m) + mn7(n*3, m)
+
+
+def mn8(n, m):
+    if n > m or n == 6 or n == 12:
+        return 0
+    elif n == m:
+        return 1
+    else:
+        return mn8(n + 1, m) + mn8(n*2, m) + mn8(n + 3, m)
 
 
 def mn9(n, m):
@@ -54,8 +79,21 @@ def mn9(n, m):
         return mn9(n+1, m) + mn9(n*2, m) + mn9(n*3, m)
 
 
+def mn10(n, m):
+    if n > m or n == 59:
+        return 0
+    elif n == m:
+        return 1
+    else:
+        return mn10(n + 1, m) + mn10(n * 2, m)
+
+
 print(1, mn1(3, 12) * mn1(12, 16))
 print(2, mn2(3, 10) * mn2(10, 25))
 print(3, mn3(4, 22))
 print(5, mn5(1, 29))
+print(6, mn6(2, 11) * mn6(11, 25) * mn6(25, 50))
+print(7, mn7(1, 10) * mn7(10, 15))
+print(8, mn8(3, 16))
 print(9, mn9(1, 13))
+print(10, mn10(3, 14) * mn10(14, 62))
